bbd1ed61040af99f2ec708f8e3bc73b72fa115f1
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-04-14 17:14:32 +0000

    alteração do JOGO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,51 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+// Área para definição das variáveis para armazenar as propriedades das cidades
+// Area para entrada de dados
+// Área para exibição dos dados da cidade
+// Área para exibição dos dados da cidade
 
-  // Área para entrada de dados
+char estado[20], codigo[10], cidade[20];//o valor entre chaves especificaa quantidade máxima  de caracteres da variável
+int pt, carta;
+float pib, area, populacao;
 
-  // Área para exibição dos dados da cidade
+printf("**************************************\n");
+printf("********* DESAFIOS CARTAS ************\n");
+printf("**************************************\n");
 
-return 0;
-} 
+printf("Escreva o numero da carta:\n");
+scanf("%i", &carta); //sintaxe "i" serve valores inteiros
+printf("Escreva o estado com uma letra(de A - H):\n");
+scanf("%s", estado);  //sintaxe "%s" serve string
+
+printf("Escreva o codigo da carta(de 01 - 04):\n");
+scanf("%s", codigo); 
+
+printf("Escreva o nome da cidade:\n");
+scanf("%s", cidade); //sintaxe "s" serve para especificar strings com mais letras
+
+printf("Escreva a população em milhares(ex:10.000):\n");
+scanf("%f", &populacao); //sintaxe "%f FLOAT para numeros quebrados
+
+pintf("Escreva a area em milhares(ex:10.000):\n");
+scanf("%f", &area);
+
+printf("Escreva o PIB em milhares(ex:10.000):\n");
+scanf("%f", &pib); //variáveis tipo float, necessitam do operador &
+
+printf("Escreva o N de pontos turisticos:\n");
+scanf("%i", &pt); //variáveis tipo int, necessitam do operador &
+
+//**********************************************************
+printf("Carta: %i \n", carta); 
+printf("Estado: %s \n", estado);
+printf("Código: %s",estado); //concatenando sem usar funções, aqui não foi feita a quebra de linha para juntar as variáveis
+printf("%s \n",codigo);//concatenando sem usar funções
+
+pintf("Nome da Cidade: %s \n", cidade);
+printf("População: %.2f \n", populacao);
+printf("Área: %.2f km²\n", area);//coloca-se  continuação do texto após a sintaxe e antes da quebra de linha
+printf("PIB: %.2f R$\n ", pib);//pib não é uma variável tipo int, portanto não precisa do &
+printf("Número de pontos turísticos: %i \n", pt);
+
