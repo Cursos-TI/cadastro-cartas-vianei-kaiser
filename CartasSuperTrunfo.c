c7fb0ff67ce691fb0a3ab51a1f8a75a191513e77
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-04-15 10:28:00 +0000

    ACERTEI O CODIGO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -47,7 +47,7 @@ int main() {
     printf("Área: %.2f km²\n", area);
     printf("PIB: %.2f R$\n", pib);
     printf("Número de pontos turísticos: %i \n", pt);
-    printf("========== FIM =============\n");
+    printf("************** FIM ************************\n");
 
     return 0; / Indica que o programa terminou corretamente
 }
