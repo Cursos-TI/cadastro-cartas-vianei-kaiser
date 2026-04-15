0503f744ce3c07d389b15d3cfd66bf713478dd13
vianei-kaiser <65637392+vianei-kaiser@users.noreply.github.com>
2026-04-15 10:17:29 +0000

    1

--- teste.c
+++ teste.c
@@ -3,7 +3,7 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 int main() {
-    printf(" -- ");
+    printf(" --dddddddddddddddddd ");
 
 
 return 0;
